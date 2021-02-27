2dc8eb5a28bbfc0026e8b3478b0e6775af00dc0b
danivelop <mofg266@gmail.com>
2021-02-27 21:13:41 +0900

    setting: (#8) media주입

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import React, { useMemo } from 'react'
-import { ThemeProvider } from 'styled-components'
+import React from 'react'
 
-import colors from 'constants/colors'
 import { GlobalStyle } from 'global-styles'
 
 function App() {
-  const theme = useMemo(
-    () => ({
-      colors,
-    }),
-    [],
-  )
-
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <div>hello world 헬로 월드</div>
       <GlobalStyle />
-    </ThemeProvider>
+    </>
   )
 }
 
